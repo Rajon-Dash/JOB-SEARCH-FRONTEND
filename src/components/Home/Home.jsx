3ff4f16b149300f7de6db3523d17aa8644f60916
Rajon Dash <cse_1922020022@lus.ac.bd>
2024-07-05 13:18:34 +0600

    commit

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { useContext } from "react";
+import { Context } from "../../main";
+import { Navigate } from "react-router-dom";
+import HeroSection from "./HeroSection";
+import HowItWorks from "./HowItWorks";
+import PopularCategories from "./PopularCategories";
+import PopularCompanies from "./PopularCompanies";
+
+const Home = () => {
+  const { isAuthorized } = useContext(Context);
+  if (!isAuthorized) {
+    return <Navigate to={"/login"} />;
+  }
+  const style ={
+    padding: "0px 80px 0px 80px"
+  }
+  return (
+    <>
+      <section className="homePage page" style={style}>
+        <HeroSection />
+        <HowItWorks />
+        <PopularCategories />
+        <PopularCompanies />
+      </section>
+    </>
+  );
+};
+
+export default Home;
